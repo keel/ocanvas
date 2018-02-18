f5d0bd5821c91463c8cb5063b45bc82e5b0e3054
keel <keel.sike@gmail.com>
2018-02-19 01:45:47 +0800

    animation add loop para

--- src/displayobjects/sprite.js
+++ src/displayobjects/sprite.js
@@ -1,240 +1,241 @@
-(function(oCanvas, window, document, undefined){
+(function(oCanvas, window, document, undefined) {
 
-	// Define the class
-	var sprite = function (settings, thecore) {
+  // Define the class
+  var sprite = function(settings, thecore) {
 
-		// Return an object when instantiated
-		return oCanvas.extend({
-			core: thecore,
+    // Return an object when instantiated
+    return oCanvas.extend({
+      core: thecore,
 
-			// Set properties
-			shapeType: "rectangular",
-			loaded: false,
-			firstDrawn: false,
-			frames: [],
-			duration: 0,
-			frame: 1,
-			generate: false,
-			numFrames: 0,
-			offset_x: 0,
-			offset_y: 0,
-			direction: "x",
-			running: false,
-			active: false,
-			loop: true,
-			clipChildren: false,
+      // Set properties
+      shapeType: "rectangular",
+      loaded: false,
+      firstDrawn: false,
+      frames: [],
+      duration: 0,
+      frame: 1,
+      generate: false,
+      numFrames: 0,
+      offset_x: 0,
+      offset_y: 0,
+      direction: "x",
+      running: false,
+      active: false,
+      loop: true,
+      clipChildren: false,
 
-			_: oCanvas.extend({}, thecore.displayObject._, {
-				autostart: false,
-				hasBeenDrawn: false
-			}),
+      _: oCanvas.extend({}, thecore.displayObject._, {
+        autostart: false,
+        hasBeenDrawn: false
+      }),
 
-			set autostart (value) {
-				this.active = value;
-				this._.autostart = value;
-			},
+      set autostart(value) {
+        this.active = value;
+        this._.autostart = value;
+      },
 
-			get autostart () {
-				return this._.autostart;
-			},
+      get autostart() {
+        return this._.autostart;
+      },
 
-			// Init method for loading the image resource
-			init: function () {
-				if (this.image === undefined) {
-					return;
-				}
-				var _this = this;
+      // Init method for loading the image resource
+      init: function() {
+        if (this.image === undefined) {
+          return;
+        }
+        var _this = this;
 
-				var isImageElement = this.image.nodeName && this.image.nodeName.toLowerCase() === "img";
-				this.img = isImageElement ? this.image : new Image();
+        var isImageElement = this.image.nodeName && this.image.nodeName.toLowerCase() === "img";
+        this.img = isImageElement ? this.image : new Image();
 
-				// Get dimensions when the image is loaded. Also, remove the temp img from DOM
-				var onload = function () {
+        // Get dimensions when the image is loaded. Also, remove the temp img from DOM
+        var onload = function() {
 
-					// Set the full source image dimensions
-					_this.full_width = this.width;
-					_this.full_height = this.height;
+          // Set the full source image dimensions
+          _this.full_width = this.width;
+          _this.full_height = this.height;
 
-					// If automatic generation is specified
-					if (_this.generate) {
-						var dir, length_full, length_cropped, num_frames, i;
+          // If automatic generation is specified
+          if (_this.generate) {
+            var dir, length_full, length_cropped, num_frames, i;
 
-						// Get frame data
-						dir = _this.direction;
-						length_full = (dir === "y") ? _this.full_height : _this.full_width;
-						length_cropped = (dir === "y") ? _this.height : _this.width;
+            // Get frame data
+            dir = _this.direction;
+            length_full = (dir === "y") ? _this.full_height : _this.full_width;
+            length_cropped = (dir === "y") ? _this.height : _this.width;
 
-						if (_this.numFrames > 0) {
-							num_frames = _this.numFrames;
-						} else {
-							num_frames = length_full / length_cropped;
-							_this.numFrames = num_frames;
-						}
+            if (_this.numFrames > 0) {
+              num_frames = _this.numFrames;
+            } else {
+              num_frames = length_full / length_cropped;
+              _this.numFrames = num_frames;
+            }
 
-						// Create frames based on the specified width, height, direction, offset and duration
-						_this.frames = [];
-						for (i = 0; i < num_frames; i++) {
-							_this.frames.push({
-								x: _this.offset_x + (i * (dir === "x" ? _this.width : 0)),
-								y: _this.offset_y + (i * (dir === "y" ? _this.height : 0)),
-								d: _this.duration
-							});
-						}
-					}
-					_this.loaded = true;
-					if (_this._.hasBeenDrawn) _this.core.redraw();
-				};
+            // Create frames based on the specified width, height, direction, offset and duration
+            _this.frames = [];
+            for (i = 0; i < num_frames; i++) {
+              _this.frames.push({
+                x: _this.offset_x + (i * (dir === "x" ? _this.width : 0)),
+                y: _this.offset_y + (i * (dir === "y" ? _this.height : 0)),
+                d: _this.duration
+              });
+            }
+          }
+          _this.loaded = true;
+          if (_this._.hasBeenDrawn) _this.core.redraw();
+        };
 
-				if (isImageElement && this.img.complete) {
-					onload.call(this.img);
-				} else {
-					this.img.addEventListener("load", onload);
+        if (isImageElement && this.img.complete) {
+          onload.call(this.img);
+        } else {
+          this.img.addEventListener("load", onload);
 
-					if (!isImageElement) {
-						this.img.src = this.image;
-					}
-				}
-			},
+          if (!isImageElement) {
+            this.img.src = this.image;
+          }
+        }
+      },
 
-			draw: function () {
-				var _this = this,
-					canvas = this.core.canvas,
-					origin = this.getOrigin(),
-					x = this.abs_x - origin.x,
-					y = this.abs_y - origin.y,
-					frame;
+      draw: function() {
+        var _this = this,
+          canvas = this.core.canvas,
+          origin = this.getOrigin(),
+          x = this.abs_x - origin.x,
+          y = this.abs_y - origin.y,
+          frame;
 
-				// If the image has not been loaded or the sprite has no frames, the frame size must be 0 (for clipChildren feature).
-				var frame_width=0, frame_height=0;
+        // If the image has not been loaded or the sprite has no frames, the frame size must be 0 (for clipChildren feature).
+        var frame_width = 0,
+          frame_height = 0;
 
-				// If the image has finished loading, go on and draw
-				if (this.loaded) {
+        // If the image has finished loading, go on and draw
+        if (this.loaded) {
 
-					// Draw current frame
-					if (this.frames.length > 0) {
+          // Draw current frame
+          if (this.frames.length > 0) {
 
-						// Get current frame
-						if (this.frame > this.frames.length) {
+            // Get current frame
+            if (this.frame > this.frames.length) {
 
-							// Do clip with an empty path
-							if(this.clipChildren) {
-								canvas.beginPath();
-								canvas.rect(x, y, 0, 0);
-								canvas.closePath();
-								canvas.clip();
-							}
+              // Do clip with an empty path
+              if (this.clipChildren) {
+                canvas.beginPath();
+                canvas.rect(x, y, 0, 0);
+                canvas.closePath();
+                canvas.clip();
+              }
 
-							return this;
+              return this;
 
-						}
+            }
 
-						frame = this.frames[this.frame - 1];
-						frame_width = (frame.w !== undefined) ? frame.w : this.width;
-						frame_height = (frame.h !== undefined) ? frame.h : this.height;
+            frame = this.frames[this.frame - 1];
+            frame_width = (frame.w !== undefined) ? frame.w : this.width;
+            frame_height = (frame.h !== undefined) ? frame.h : this.height;
 
-						// Draw the current sprite part
-						canvas.drawImage(this.img, frame.x, frame.y, frame_width, frame_height, x, y, frame_width, frame_height);
+            // Draw the current sprite part
+            canvas.drawImage(this.img, frame.x, frame.y, frame_width, frame_height, x, y, frame_width, frame_height);
 
-						// Do stroke if stroke width is specified
-						if (this.strokeWidth > 0) {
-							canvas.lineWidth = this.strokeWidth;
-							canvas.strokeStyle = this.strokeColor;
-							canvas.strokeRect(x, y, frame_width, frame_height);
-						}
+            // Do stroke if stroke width is specified
+            if (this.strokeWidth > 0) {
+              canvas.lineWidth = this.strokeWidth;
+              canvas.strokeStyle = this.strokeColor;
+              canvas.strokeRect(x, y, frame_width, frame_height);
+            }
 
-						// Set a redraw timer at the current frame duration if a timer is not already running
-						if (this.running === false && this.active) {
+            // Set a redraw timer at the current frame duration if a timer is not already running
+            if (this.running === false && this.active) {
 
-							// setTimeout(function () {
+              // setTimeout(function () {
 
-							var nextFrame = function(ts) {
-								if (!_this.startTime) {
-									_this.startTime = ts;
-								}
-								if (ts - _this.startTime <  frame.d) {
-									requestAnimationFrame(nextFrame);
-									return;
-								}
-								_this.startTime = ts;
-								// Increment the frame number only after the frame duration has passed
-								if (_this.loop) {
-									_this.frame = (_this.frame === _this.frames.length) ? 1 : _this.frame + 1;
-								} else {
-									_this.frame = (_this.frame === _this.frames.length) ? _this.frame : _this.frame + 1;
-								}
+              var nextFrame = function(ts) {
+                if (!_this.startTime) {
+                  _this.startTime = ts;
+                }
+                if (ts - _this.startTime < frame.d) {
+                  requestAnimationFrame(nextFrame);
+                  return;
+                }
+                _this.startTime = ts;
+                // Increment the frame number only after the frame duration has passed
+                if (_this.loop) {
+                  _this.frame = (_this.frame === _this.frames.length) ? 1 : _this.frame + 1;
+                } else {
+                  _this.frame = (_this.frame === _this.frames.length) ? _this.frame : _this.frame + 1;
+                }
 
-								// Set timer status
-								_this.running = false;
+                // Set timer status
+                _this.running = false;
 
-								// Redraw canvas if the timeline is not running
-								if (!_this.core.timeline.running) {
-									_this.core.draw.redraw();
-								}
-							};
-							requestAnimationFrame(nextFrame);
+                // Redraw canvas if the timeline is not running
+                if (!_this.core.timeline.running) {
+                  _this.core.draw.redraw();
+                }
+              };
+              requestAnimationFrame(nextFrame);
 
-							// }, frame.d);
+              // }, frame.d);
 
-							// Set timer status
-							this.running = true;
-						}
-					}
+              // Set timer status
+              this.running = true;
+            }
+          }
 
-					// Clear the timer if this is the first time it is drawn
-					if (this.firstDrawn === false) {
-						this.firstDrawn = true;
-						clearTimeout(this.loadtimer);
-					}
-				}
+          // Clear the timer if this is the first time it is drawn
+          if (this.firstDrawn === false) {
+            this.firstDrawn = true;
+            clearTimeout(this.loadtimer);
+          }
+        }
 
-				// If the image hasn't finished loading, set a timer and try again
-				else {
-					clearTimeout(this.loadtimer);
-					this.loadtimer = setTimeout(function () {
-						_this.draw();
-					}, 100);
-				}
+        // If the image hasn't finished loading, set a timer and try again
+        else {
+          clearTimeout(this.loadtimer);
+          this.loadtimer = setTimeout(function() {
+            _this.draw();
+          }, 100);
+        }
 
-				// Do clip
-				if(this.clipChildren) {
+        // Do clip
+        if (this.clipChildren) {
 
-					// draw the clip region (the square area representing the current frame)
-					canvas.beginPath();
-					canvas.rect(x, y, frame_width, frame_height);
-					canvas.closePath();
+          // draw the clip region (the square area representing the current frame)
+          canvas.beginPath();
+          canvas.rect(x, y, frame_width, frame_height);
+          canvas.closePath();
 
-					canvas.clip();
+          canvas.clip();
 
-				}
+        }
 
-				this._.hasBeenDrawn = true;
+        this._.hasBeenDrawn = true;
 
-				return this;
-			},
+        return this;
+      },
 
-			start: function () {
-				this.startAnimation();
+      start: function() {
+        this.startAnimation();
 
-				return this;
-			},
+        return this;
+      },
 
-			startAnimation: function () {
-				this.active = true;
-				this.core.redraw();
+      startAnimation: function() {
+        this.active = true;
+        this.core.redraw();
 
-				return this;
-			},
+        return this;
+      },
 
-			stopAnimation: function () {
-				this.active = false;
+      stopAnimation: function() {
+        this.active = false;
 
-				return this;
-			},
+        return this;
+      },
 
-		}, settings);
-	};
+    }, settings);
+  };
 
-	// Register the display object
-	oCanvas.registerDisplayObject("sprite", sprite, "init");
+  // Register the display object
+  oCanvas.registerDisplayObject("sprite", sprite, "init");
 
 })(oCanvas, window, document);
